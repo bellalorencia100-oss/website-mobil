ca4188bc3542c39fe8826597eafb83d7deec254f
Kristian <bellalorencia18@gmail.com>
2026-09-16 11:35:16 +0700

    ...

--- admin-mobilku/src/pages/KelolaMobil.jsx
+++ admin-mobilku/src/pages/KelolaMobil.jsx
@@ -413,7 +413,7 @@ const KelolaMobil = () => {
           </div>
 
           {isSubmitting ? (
-            <div className="flex-1 flex justify-center items-center bg-black/10">
+            <div className="flex-1 flex justify-center items-center">
               <span className="loading loading-spinner loading-lg"></span>
             </div>
           ) : (
