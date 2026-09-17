9e5972205f4d56ba7239e9d19a11bc9e29825921
Kristian <bellalorencia18@gmail.com>
2026-09-17 12:00:13 +0700

    update: perubahan ketiga

--- client/src/components/CategoryCard.jsx
+++ client/src/components/CategoryCard.jsx
@@ -20,8 +20,8 @@ function CategoryCard({ name, logo, categoryId }) {
   return (
     <div className="flex flex-col items-center gap-2">
       <Link to={target} onClick={handleClick}>
-        <div className="w-14 h-14 bg-white rounded-lg shadow-sm border border-gray-100 flex items-center justify-center p-2 transition hover:shadow-md hover:border-red-300 hover:scale-110 active:shadow-md active:border-red-300 active:scale-110">
-          <img src={logo} className="w-8 h-8 object-contain" />
+        <div className="w-14 h-14 bg-white rounded-lg shadow-sm border border-gray-100 flex items-center justify-center overflow-hidden p-2 transition hover:shadow-md hover:border-red-300 hover:scale-110 active:shadow-md active:border-red-300 active:scale-110">
+          <img src={logo} className="w-7 h-7 object-contain" />
         </div>
       </Link>
       <span className="text-xs text-center w-14">{name}</span>
