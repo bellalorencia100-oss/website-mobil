6b86332a77b07c7a79d5d8332316982cf01da56e
Kristian <bellalorencia18@gmail.com>
2026-09-17 18:05:40 +0700

    update: darkomode non aktif dan menaikan sedikit di bagian pencarian box

--- client/src/pages/Katalog.jsx
+++ client/src/pages/Katalog.jsx
@@ -195,7 +195,7 @@ function Katalog() {
     <>
       <Navbar />
       <div className="bg-white rounded-lg">
-        <div className="max-w-4xl mx-auto mt-10 px-3 relative" ref={boxRef}>
+        <div className="max-w-4xl mx-auto mt-6 mb-6 px-3 relative" ref={boxRef}>
           <input
             type="text"
             placeholder="Cari mobil"
@@ -271,8 +271,8 @@ function Katalog() {
           )}
         </div>
 
-        <div className="max-w-4xl mx-auto px-3 relative mt-4">
-          <div className="flex gap-2 overflow-x-auto justify-between">
+        <div className="max-w-4xl mx-auto px-3 relative mt-6">
+          <div className="flex gap-2 overflow-x-auto justify-between pb-3">
             <button
               className="px-9 py-2 rounded-lg bg-red-700 border text-white text-sm font-semibold"
               onClick={() => handleFilterClick({})}
